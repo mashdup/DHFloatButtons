2d4dc79f1299bcbed0617d49b5676ac77e14388d
Dillon Hoa <mr_d_2chat@hotmail.com>
2018-02-16 14:26:22 +0000

    Initial Commit

---
+++ DHFloatButtons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DHFloatButtons
+//
+//  Created by Dillon on 16/02/2018.
+//  Copyright © 2018 8bitDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DHFloatButtons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DHFloatButtons
+//
+//  Created by Dillon on 16/02/2018.
+//  Copyright © 2018 8bitDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
